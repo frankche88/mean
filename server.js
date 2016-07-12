8ca9f2561d63f7583193f8950cfb77f12121831f
Admin <admin@A51-05.local>
2016-07-11 20:24:16 -0500

    adding routes

--- server.js
+++ server.js
@@ -2,9 +2,53 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var adminRouter = express.Router();
+
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname)+ '/index.html');
 });
+
+//Middleware
+adminRouter.use(function (req,res,next){
+
+  console.log('--->',req.method,req.url);
+
+  next();
+});
+
+adminRouter.param('name',function(req,resp,next,name){
+console.log("req.name",req.name);
+console.log("name",name);
+
+req.name = "Mr robot was here";
+
+next();
+});
+
+//Rutas
+adminRouter.get('/',function(req,res){
+res.send("I am in the first page");
+
+});
+
+adminRouter.get('/users',function(req,res){
+  console.log('llegue a la vista usuarios');
+res.send("users");
+
+});
+
+adminRouter.get('/users/:name',function(req,res){
+
+res.send("hola "+req.name);
+
+});
+
+adminRouter.get('/posts',function(req,res){
+res.send("posts");
+
+});
+
+app.use('/admin', adminRouter);
 
 app.set('port',(process.env.PORT || 5000));
 
