6d0071f07e968f6f48f86b2f0b87b435348daa4a
Admin <admin@A51-05.local>
2016-07-11 21:36:46 -0500

    adding login validation

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ var app = express();
 var path = require('path');
 
 var adminRouter = express.Router();
+var loginRouter = express.Router();
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname)+ '/index.html');
+});
+app.get('/error',function(req,res){
+  res.sendFile(path.join(__dirname)+ '/error.html');
 });
 
 //Middleware
@@ -25,30 +29,69 @@ req.name = "Mr robot was here";
 next();
 });
 
+loginRouter.param('password',function(req,resp,next,password){
+
+req.isvalidPassword = password == "123";
+
+console.log("password",password);
+
+if(!req.isvalidPassword){
+  resp.redirect('/error');
+}else
+next();
+
+});
+
+loginRouter.param('name',function(req,resp,next,name){
+
+req.name = name;
+
+req.isvalidName = name == "henry";
+
+if(!req.isvalidName){
+  resp.redirect('/error');
+}
+else {
+  next();
+}
+
+});
+
 //Rutas
 adminRouter.get('/',function(req,res){
 res.send("I am in the first page");
-
 });
 
 adminRouter.get('/users',function(req,res){
   console.log('llegue a la vista usuarios');
-res.send("users");
-
+  res.send("users");
 });
 
 adminRouter.get('/users/:name',function(req,res){
-
 res.send("hola "+req.name);
-
 });
 
 adminRouter.get('/posts',function(req,res){
 res.send("posts");
+});
 
+loginRouter.get('/',function(req,res){
+res.send("I am in login");
+});
+
+loginRouter.get('/users/:name',function(req,res){
+var mensaje = req.isvalid ? "welcome " + req.name: "usuario invalido";
+res.send(mensaje);
+});
+
+loginRouter.get('/authenticate/:name/:password',function(req,res){
+
+    res.send("auth welcome " + req.name);
 });
 
 app.use('/admin', adminRouter);
+
+app.use('/login', loginRouter);
 
 app.set('port',(process.env.PORT || 5000));
 
