e0ceff068a41f4db5be523875f6365c987dd2c46
Admin <admin@A51-05.local>
2016-07-25 22:11:21 -0500

    adding angular

---
+++ angular_sample/js/controllers.js
@@ -0,0 +1,29 @@
+angular.module('myApp.controllers',[])
+.controller('mainCtrl', function($scope,$filter) {
+
+    $scope.message = 'la aplicacion ha sido creada';
+    $scope.name ='henry fuentes';
+    $scope.toLowerCase =$filter('lowercase')($scope.name);
+    console.log($scope.message);
+
+})
+.controller('clockCtrl', function($scope) {
+
+    $scope.clock ={
+      now:new Date()
+    }
+
+    var updateClock = function () {
+
+      $scope.clock.now = new Date();
+    }
+
+    $scope.changeClock = function () {
+
+      updateClock();
+    }
+
+    setInterval(function(){
+    $scope.$apply(updateClock);
+  },1000);
+})
