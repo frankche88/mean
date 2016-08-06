32eaefc7c815676876eb9687177944fcd95290a1
Admin <admin@A51-05.local>
2016-08-05 21:56:01 -0500

    ini view pokemons

--- public/js/app.controllers.js
+++ public/js/app.controllers.js
@@ -1,13 +1,46 @@
 angular.module('pokeApp.controllers', [])
-    .controller('loginCtrl', function() {
+    .controller('loginCtrl', function($http, pokemonServices) {
         var vm = this;
         vm.message = "Este es el login";
+
     })
     .controller('userCtrl', function() {
         var vm = this;
         vm.message = "Este es el admin usuario";
     })
-    .controller('pokemonCtrl', function() {
-        var vm = this;
-        vm.message = "Este es el admin pokemon";
+    .controller('pokemonCtrl', function(pokemonServices,LxDialogService, LxNotificationService) {
+            var vm = this;
+
+            vm.dialogId = 'dialog-test';
+
+            vm.infoPokemon = {};
+
+            var getPokemons = function() {
+
+                pokemonServices.getPokemons().then(
+                    function(response) {
+                        vm.pokemons = response;
+                    },
+                    function(response) {
+
+                    });
+            };
+
+            vm.search = function(){
+
+              pokemonServices.search(vm.searchPokemon).then(
+                  function(response) {
+                      vm.pokemons = response;
+                  },
+                  function(response) {
+
+                  });
+            };
+
+            vm.openDialog = function(infoPokemon){
+              vm.infoPokemon = infoPokemon;
+               LxDialogService.open(vm.dialogId);
+            }
+
+            getPokemons();
     });
--- public/js/app.services.js
+++ public/js/app.services.js
@@ -1,1 +1,39 @@
-angular.module('pokeApp.services', []);
+angular.module('pokeApp.services', [])
+    .service('pokemonServices', function($http,$q,$filter) {
+
+        var _pokemons = undefined;
+
+        this.iniData = function(){
+
+          if(!_pokemons){
+
+            var deferred = $q.defer();
+
+                  $http.get("bd_pokemon/pokemons.json")
+                     .success(function (response) {
+                         deferred.resolve(response);
+                     })
+                     .error(function (response) {
+                         deferred.reject(response);
+                     });
+
+             _pokemons = deferred.promise;
+          }
+        };
+
+        this.getPokemons = function() {
+
+            return _pokemons;
+        };
+
+        this.search = function(namePokemon) {
+
+          var deferred = $q.defer();
+
+          var found = $filter('filter')(_pokemons.$$state.value, { name: namePokemon}, false);
+            deferred.resolve(found);
+           return deferred.promise;
+        };
+
+        this.iniData();
+    });
